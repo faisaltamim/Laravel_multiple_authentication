62ae9aa04195aaf461b1787f359b0bdff7558a8e
faisaltamimPro <mdf41401@gmail.com>
2021-02-28 20:02:12 +0600

    basic multi auth succes next advance

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -11,8 +12,15 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get( '/', function () {
+    return view( 'welcome' );
+} );
+
+Auth::routes();
+
+Route::get( '/home', 'HomeController@index' )->name( 'home' );
+
+//this route for admin
+Route::get( 'admin/home', 'HomeController@adminHome' )->name( 'admin.home' )->middleware( 'is_admin' );
